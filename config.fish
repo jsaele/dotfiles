b66e27344afb2369d983e1d8ee83d4c32e266f9e
Joakim Sælemyr <joakim.saelemyr@gmail.com>
2017-03-06 21:06:47 +0100

    added some stuff

---
+++ config.fish
@@ -0,0 +1,3 @@
+#function fish_prompt
+#    source ~/.local/lib/python2.7/site-packages/powerline/bindings/fish/powerline-setup.fish
+#end
--- linkIt.sh
+++ linkIt.sh
@@ -5,3 +5,7 @@
 
 cd
 ln -s $1/dotfiles/.gitconfig .gitconfig
+ln -s $1/dotfiles/.profile .profile
+# maybe not the best solution
+mkdir .config && mkdir .config/fish && ln -s ../../$1/dotfiles/config.fish .config/fish/config.fish
+mkdir .fonts && ln -s ../$1/dotfiles/PowerlineSymbols.otf .fonts/PowerlineSymbols.otf
