feb0a7daa8117c2345c6877bd0fe8108d6e5a17a
Joakim Sælemyr <joakim.saelemyr@gmail.com>
2017-04-26 12:55:09 +0200

    added i3status config

--- linkIt.sh
+++ linkIt.sh
@@ -10,4 +10,5 @@ ln -s $1/dotfiles/.profile .profile
 mkdir .config && mkdir .config/fish && ln -s ../../$1/dotfiles/config.fish .config/fish/config.fish
 mkdir .config/terminator && ln -s ../../$1/dotfiles/terminator-config .config/terminator/config
 mkdir .config/i3 && ln -s ../../$1/dotfiles/i3-config .config/i3/config
+mkdir .config/i3status && ln -s ../../$1/dotfiles/i3status-config .config/i3status/config
 mkdir .fonts && ln -s ../$1/dotfiles/PowerlineSymbols.otf .fonts/PowerlineSymbols.otf
