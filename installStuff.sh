3c4ab489715e089fbf97d65a9f615a8b60396fb7
Joakim Sælemyr <joakim.saelemyr@gmail.com>
2017-07-10 09:17:49 +0200

    config stuff

--- installStuff.sh
+++ installStuff.sh
@@ -1,28 +1,72 @@
-#!/bin/sh
+s#!/bin/sh
+
+# Emacs
+
+if [ !emacs25 ]
+then    
+    sudo add-apt-repository ppa:kelleyk/emacs
+    sudo apt-get update
+    sudo apt-get install emacs25 -y
+fi
 
 
-# NodeJS
+# i3
 
-if [ ! node ]
+if [ !i3 ]
 then
-    sudo apt install nodejs
-    sudo apt install npm
+    sudo apt install i3 -y
+fi
+
+# playerctl for spotify and other stuff volume controls. command not working properly
+if [ !playerctl ]
+then
+    wget https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb | sudo dpkg -i playerctl-0.5.0_amd64.deb 
+fi
+
+# Terminator
+
+if [ !terminator ]
+then
+    sudo apt-get update 
+    sudo apt-get install terminator -y
+fi
+
+
+# Fish 
+if [ !fish ]
+then
+    wget https://launchpad.net/~fish-shell/+archive/ubuntu/release-2/+files/fish_2.6.0.orig.tar.gz -O fish.tar.gz
+    sudo dpkg -i fish.deb
+
+
+# Curl
+
+if [ !curl ]
+then
+    sudo apt install curl
+
+# NodeJS & npm
+
+if [ !node ]
+then
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get install -y nodejs
 fi
 
 
 # TypeScript
 
-if [ ! tsc ]
-then
-    sudo npm install -g typescript
-fi
+#if [ ! tsc ]
+#then
+#    sudo npm install -g typescript
+#fi
 
 # Elm
 
-if [ ! elm ]
+if [ !elm ]
 then
     #elm
-    sudo npm install -g elm
+    sudo npm install -g elm -y
 
     #elm-format -- improve with version latest
     wget https://github.com/avh4/elm-format/releases/download/0.6.0-alpha/elm-format-0.18-0.6.0-alpha-linux-x64.tgz -O temp.tgz;
@@ -38,10 +82,10 @@ fi
 
 # Elixir
 
-if [ ! elixir ]
+if [ !elixir ]
 then
     wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb; # TODO: What about versions?
     sudo apt-get update;
-    sudo apt-get install esl-erlang;
-    sudo apt-get install elixir;
+    sudo apt-get install esl-erlang -y;
+    sudo apt-get install elixir -y;
 fi
