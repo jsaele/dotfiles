4d189bdcad68087a1a9b32f0b24627bca179f422
Joakim Sælemyr <joakim.saelemyr@gmail.com>
2024-03-04 14:03:06 +0100

    awdwa

--- linkIt.sh
+++ linkIt.sh
@@ -7,6 +7,7 @@ cd
 ln -s $1/dotfiles/.gitconfig .gitconfig
 ln -s $1/ohai-emacs/ .emacs.d
 ln -s $1/dotfiles/.profile .profile
+ln -s $1/dotfiles/.zprofile .zprofile
 # maybe not the best solution
 mkdir -p .config/fish && ln -s ../../$1/dotfiles/config.fish .config/fish/config.fish
 mkdir -p .config/terminator && ln -s ../../$1/dotfiles/terminator-config .config/terminator/config
